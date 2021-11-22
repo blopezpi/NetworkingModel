32a298ddd79dd5cf9c95b381ca3bd2af54d4fc0b
Roberto <roberto.rodriguez@tokiota.com>
2021-11-22 11:31:11 +0100

    ok

--- modules/rg/main.tf
+++ modules/rg/main.tf
@@ -1,8 +1,6 @@
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
+resource "azurerm_resource_group" "rg" {
+  name                = var.rg_name
   location            = var.location
-  rg_name             = var.rg_name
-  address_space       = var.address_space
 #  tags                = local.tags
 
 #   dns_servers = coalesce(
