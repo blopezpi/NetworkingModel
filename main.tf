c2ef9af7c1b46fdcaabd1b4a2df67d0b9fc9531b
Roberto <roberto.rodriguez@tokiota.com>
2021-11-22 10:02:19 +0100

    ok

--- main.tf
+++ main.tf
@@ -11,9 +11,13 @@ module "resource_groups" {
 module "vnet" {
     count = length(var.address_space)
     source = "./modules/vnet"
-    rg_name = var.resource_group_name
+    rg_name = module.resource_groups.rg_name
     vnet_name = var.virtual_network_name
+<<<<<<< HEAD:main.tf
     address_space = var.address_space[count.index]
+=======
+    address_space = var.address_space
+>>>>>>> ok:modules/main.tf
     location = var.location
 }
 
---
+++ modules/vnet/variables.tf
@@ -0,0 +1,8 @@
+variable "vnet" {
+  default = ""
+}
+
+variable "address_space" {
+  description = "The address space that is used by the network."
+  default     = [""]
+}
