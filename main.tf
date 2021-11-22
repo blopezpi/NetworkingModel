752b631505a1f969aaf2ecd3cb6f30d48ba5f07e
Roberto <roberto.rodriguez@tokiota.com>
2021-11-22 11:10:20 +0100

    ok

--- main.tf
+++ main.tf
@@ -5,14 +5,14 @@ provider "azurerm" {
     }
 module "resource_groups" {
   source = "./modules/rg"
-  rg_name = var.resource_group_name
+  rg_name = var.rg_name
   location = var.location
 }
 
 module "vnets" {
     count = length(var.address_space)
     source = "./modules/vnet"
-    rg_name = module.resource_groups.rg_name
+    rg_name = module.rg_name
     vnet_name = var.virtual_network_name
     location = var.location
     address_space = var.address_space[count.index]
