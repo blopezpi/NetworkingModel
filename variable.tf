9e66a1ee97502efb90e9804c45fd7f8efdfd6b3e
Roberto <roberto.rodriguez@tokiota.com>
2021-11-22 09:55:32 +0100

    ok

--- variable.tf
+++ variable.tf
@@ -1,6 +1,11 @@
 variable "resource_group_name" {
-default = "rg_tkt"
+default = "rg_tkt_project"
 }
 variable "location" {
 default = "westeurope"
+}
+
+variable "address_space" {
+  description = "The address space that is used by the network."
+  default     = ["172.21.0.0/16", "172.22.0.0/16", "172.23.0.0/16"]
 }
