da63afac622d01055bcfe85ae598496290f23037
Roberto <roberto.rodriguez@tokiota.com>
2021-11-22 12:15:44 +0100

    ok

--- main.tf
+++ main.tf
@@ -7,14 +7,19 @@ module "resource_groups" {
   source = "./modules/rg"
   rg_name = var.rg_name
   location = var.location
+  output "rg-name" {
+    value = azurerm_resource_group.rg.name
+  }
 }
+
+
 
 module "vnets" {
     
     count = length(var.address_space)
     depends_on = [module.resource_groups]
     source = "./modules/vnet"
-    rg_name = module.resource_groups.azurerm_resource_group.rg.name
+    rg_name = module.resource_groups.rg-name
     vnet_name = var.vnet_name
     location = var.location
     address_space = var.address_space[count.index]
