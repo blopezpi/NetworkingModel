795ab7976d41933d7a5080a218da68b732477dfe
Roberto <roberto.rodriguez@tokiota.com>
2021-11-22 12:30:07 +0100

    ok

--- variables.tf
+++ variables.tf
@@ -6,9 +6,9 @@ default = "westeurope"
 }
 
 variable "address_space" {
-  type          = list(string)
+  type          = list
   description   = "The address space that is used by the network."
-  default       = "172.21.0.0/16", "172.22.0.0/16", "172.23.0.0/16"
+  default       = ["172.21.0.0/16", "172.22.0.0/16", "172.23.0.0/16"]
 }
 
 variable "vnet_name" {
