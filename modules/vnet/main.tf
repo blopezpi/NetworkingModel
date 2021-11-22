86c13d807fb94666d45bc4af73eb4568f1d791aa
Roberto <roberto.rodriguez@tokiota.com>
2021-11-22 11:16:59 +0100

    ok

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet.name
   location            = var.location
-  resource_group_name = var.resource_group_name
-  address_space       = var.vnet.address_space
+  rg_name             = var.rg_name
+  address_space       = var.address_space
 
   }
 
