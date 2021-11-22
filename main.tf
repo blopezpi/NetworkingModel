08615bdda043f8a179384365671033a2d6b3595f
Roberto <roberto.rodriguez@tokiota.com>
2021-11-22 10:42:56 +0100

    ok

--- main.tf
+++ main.tf
@@ -7,27 +7,17 @@ module "resource_groups" {
   source = "./modules/rg"
   rg_name = var.resource_group_name
   location = var.location
-<<<<<<< HEAD:main.tf
-
-module "vnet" {
-    count = length(var.address_space)
-=======
 }
+
+
 module "vnets" {
->>>>>>> ok:modules/main.tf
+    count = length(var.address_space)
     source = "./modules/vnet"
     rg_name = module.resource_groups.rg_name
     vnet_name = var.virtual_network_name
-<<<<<<< HEAD:main.tf
-    address_space = var.address_space[count.index]
-=======
-    address_space = var.address_space
->>>>>>> ok:modules/main.tf
     location = var.location
-}
+    address_space = var.address_space[count.index]
 
-<<<<<<< HEAD:main.tf
-}
-=======
->>>>>>> ok:modules/main.tf
+
+
 
