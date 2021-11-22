bcc6de32b3fd3a1e125fd59cdf34c80cd32efac3
Roberto <roberto.rodriguez@tokiota.com>
2021-11-22 09:57:26 +0100

    ok

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "azurerm" {
+      #version = "~> 2.81.0"
+      features {
+      }
+    }
+module "resource_groups" {
+  source = "./modules/rg"
+  rg_name = var.resource_group_name
+  location = var.location
+
+module "vnet" {
+    count = length(var.address_space)
+    source = "./modules/vnet"
+    rg_name = var.resource_group_name
+    vnet_name = var.virtual_network_name
+    address_space = var.address_space[count.index]
+    location = var.location
+}
+
+}
+
---
+++ modules/rg/main.tf
@@ -0,0 +1,20 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.vnet.address_space
+#  tags                = local.tags
+
+#   dns_servers = coalesce(
+#     try(lookup(var.settings.vnet, "dns_servers", null)),
+#     try(local.dns_servers_process, null)
+#   )
+
+#   dynamic "ddos_protection_plan" {
+#     for_each = var.ddos_id != "" ? [1] : []
+
+#     content {
+#       id     = var.ddos_id
+#       enable = true
+#     }
+#   }
